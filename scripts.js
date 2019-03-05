5958d1e2305535f2ffac852abc051ab5feeac380
UsuallyChris <schoonc@gmail.com>
2019-03-04 18:44:46 -0800

    Connection to API is working

--- scripts.js
+++ scripts.js
@@ -2,10 +2,18 @@
 let request = new XMLHttpRequest();
 
 // Open a new connection. We are trying to get data, so use GET request
-request.open('GET', 'https://ghibli.herokuapp.com/films', true);
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 // Access JSON data and function to use it
-request.onload() = function() {
+request.onload = function() {
+
+  // Declare a data variable that is the parsed JSON file
+  let data = JSON.parse(this.response);
+
+  // What to do with the data variable that contains the parsed JSON
+  data.forEach(movie => {
+    console.log(movie.title);
+  });
 
 }
 
